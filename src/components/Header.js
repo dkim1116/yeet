948990b04cc3c7c8c2c340b635c2da04ea84c498
dkim1116 <daniel.kim1116@gmail.com>
2019-01-20 18:22:34 -0800

    Including general layout for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div class="ui pointing menu">
+            <a class="active item">
+                Home
+            </a>
+            <a class="item">
+                Yeets
+            </a>
+        </div>
+    );
 }
 
 export default Header;
