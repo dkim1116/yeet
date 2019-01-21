bfbcc90b59aa5f0edd0f9c8bb91eda6bbdc7f9d2
dkim1116 <daniel.kim1116@gmail.com>
2019-01-20 17:06:15 -0800

    seperated configuration related settings to a different file

---
+++ src/apis/config.js
@@ -0,0 +1,3 @@
+export const FLICKR_PUBLIC = 'fbdc58683e712c507e300e24af6ee459&format=json&nojsoncallback=1';
+export const FLICKR_FORMAT = '&format=json&nojsoncallback=1';
+export const FLICKR_TEXT = '&text=';
--- src/apis/flickr.js
+++ src/apis/flickr.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
+import { 
+    FLICKR_PUBLIC,
+    FLICKR_FORMAT,
+    FLICKR_TEXT
+} from './config';
+
 export const GetRecent = axios.create({
-    baseURL: 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=&format=json&nojsoncallback=1'
+    baseURL: `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${FLICKR_PUBLIC+FLICKR_FORMAT}`
 });
 
 export const SearchPhoto = axios.create({
-    baseURL: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=&format=json&nojsoncallback=1&text='
+    baseURL: `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_PUBLIC+FLICKR_FORMAT+FLICKR_TEXT}`
 });
